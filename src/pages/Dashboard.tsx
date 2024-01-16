2b741759d675c0d673ebb4068ffd2f027f6860e2
anshu7sah <anshu7sah@gmail.com>
2024-01-17 02:36:38 +0545

    solved routing error

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import Datagrid from "../components/DataGrid";
 import DepartmentList from "../components/DepartmentList";
 import { data } from "../data/data";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const storedUserData = localStorage.getItem("userData");
+    if (!storedUserData) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <>
       <Datagrid />
