ef55a6b542c643a916b30f4f3f28ba402e0c82ab
anshu7sah <anshu7sah@gmail.com>
2024-01-17 01:43:34 +0545

    testing route

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,8 @@
-import { useEffect } from "react";
 import Datagrid from "../components/DataGrid";
 import DepartmentList from "../components/DepartmentList";
 import { data } from "../data/data";
-import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const storedUserData = localStorage.getItem("userData");
-    if (!storedUserData) {
-      navigate("/");
-    }
-  }, []);
   return (
     <>
       <Datagrid />
